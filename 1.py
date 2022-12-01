c76b91c07496ebe14a9b67af3ee699ccf45aae42
Vince <vpurchasing@gmail.com>
2022-12-01 09:34:02 +0000

    Added solution to Day 1

--- 1.py
+++ 1.py
@@ -1,3 +1,37 @@
-def TBC():
-    pass
-    
+'''
+Part 1
+Read through the file line by line.
+If value exists, add to running sum.
+If no value (a blank line) then append the running sum to a list of sums and reset the running sum.
+'''
+
+sums = list()
+runningSum = 0
+
+with open ("1input.txt", "r") as myfile:
+    for line in myfile:
+        if line == '\n':
+            #skip
+            sums.append(runningSum)
+            runningSum = 0
+        else:
+            runningSum += int(line.strip())
+
+answer1 = (max(sums))
+print(answer1)
+
+'''
+Part 2
+Use python's max() function to find top value, then remove it from the list.
+Do this three times. Still O(n).
+'''
+
+answer2 = 0
+cur_max = answer1
+
+for _ in range(3):
+    answer2 += cur_max
+    sums.remove(cur_max)
+    cur_max = max(sums)
+
+print(answer2)
